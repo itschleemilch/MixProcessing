86fb0c5e956cbf7481acbf788cf53120d45eb1bb
Sebastian Schleemilch <github@it-schleemilch.de>
2014-08-18 13:10:04 +0200

    Moved JarManagement to new Package
    From de.itschleemilch.mixprocessing
    To de.itschleemilch.mixprocessing.load

--- source/MixProcessing/src/de/itschleemilch/mixprocessing/Main.java
+++ source/MixProcessing/src/de/itschleemilch/mixprocessing/Main.java
@@ -20,6 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package de.itschleemilch.mixprocessing;
 
+import de.itschleemilch.mixprocessing.load.JarManagement;
 import de.itschleemilch.mixprocessing.sketches.Sketch;
 import de.itschleemilch.mixprocessing.sketches.Sketches;
 import java.awt.BorderLayout;
