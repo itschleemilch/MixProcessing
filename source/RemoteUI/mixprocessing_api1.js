ef3f3947a9ee1c5e9418ff69125054dd6e90da99
Sebastian Schleemilch <github@it-schleemilch.de>
2014-08-21 06:10:33 +0200

    updated remote api template

--- source/RemoteUI/mixprocessing_api1.js
+++ source/RemoteUI/mixprocessing_api1.js
@@ -1,3 +1,27 @@
+/*
+MixProcessing - Live Mixing of Processing Sketches 
+https://github.com/itschleemilch/MixProcessing
+
+Copyright (c) 2014 Sebastian Schleemilch
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * JS Library to do the basic communication with the MixProcessing Control Server
+ *
+ */
 function log(output) {
 	console.log(output);
 }
@@ -8,8 +32,8 @@ function ajaxCallback() {
     window[jsonData.callback](jsonData);
 }
 
-function ApiCall(callBackFunction, remoteScript) {
-    var rxUrl = "api/api1/" + callBackFunction + "?" + escape(remoteScript);
+function ApiCall(remoteCall, callBackFunction) {
+    var rxUrl = "api/api1/" + callBackFunction + "?" + escape(remoteCall);
 	ajax.open("get", rxUrl, true);
 	ajax.send();
 }
@@ -24,5 +48,3 @@ else {
 	ajax = new XMLHttpRequest();
 	ajax.onload = ajaxCallback;
 }
-
-function test1Callback(json) { alert( JSON.stringify(json) );}
